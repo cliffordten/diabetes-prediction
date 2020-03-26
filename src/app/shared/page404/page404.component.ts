ce009adebf42791b8688d2f348f09e87bcb00f5b
cliffordten <tenengclifford5@gmail.com>
2020-03-26 18:36:53 +0100

    created the shared folder with 404 page not found component

---
+++ src/app/shared/page404/page404.component.ts
@@ -0,0 +1,27 @@
+import * as $ from 'jquery';
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-page404',
+  templateUrl: './page404.component.html',
+  styleUrls: ['./page404.component.css']
+})
+export class Page404Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+    $(document).ready(function(){
+
+
+    $('.no-bg').click(function(){
+      $('header').css("background-color","transparent");
+    })
+
+})
+
+  }
+
+}
