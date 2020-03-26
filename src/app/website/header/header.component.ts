b602054c3ad493c8ec8e08f8799fd356aae9f446
cliffordten <tenengclifford5@gmail.com>
2020-03-26 18:39:02 +0100

    Modified header and home section of the website

--- src/app/website/header/header.component.ts
+++ src/app/website/header/header.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  showHead: boolean = true;
 
-  constructor() { }
+  constructor() {
+
+
+}
+
 
   ngOnInit() {
+    $(document).ready(function(){
+
+       //adding sticky class to the header on scroll
+        $(window).scroll(function() {
+          if (($("header").offset().top === 0)) {
+            $("header").toggleClass("sticky");
+
+          }
+        });
+
+        if(!($('body').has("#slider-main").length == 0)){
+
+          $('header').css("background-color","transparent");
+        }
+
+        $('.no-bg').click(function(){
+          $('header').css("background-color","transparent");
+        })
+
+        $('.bg').click(function(){
+          $('header').css("background-color","#444");
+          window.location.reload()
+        })
+
+        $('.btn.btn-primary.lgn').click(function(){
+          window.location.reload()
+          $('header').css("background-color","#444");
+        })
+    });
+
   }
+
 
 }
