2a70bd12fe9c96b2635444629b7e582b937596af
cliffordten <tenengclifford5@gmail.com>
2020-03-27 11:56:15 +0100

    website signup modified

--- src/app/website/signup/signup.component.ts
+++ src/app/website/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
+
 
 
 @Component({
@@ -12,36 +12,6 @@ export class SignupComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    $(document).ready(function(){
 
-      $("li.acc-icon").click(function(){
-
-        if((!$("li.acc-icon").hasClass("active"))){
-          $("li.acc-icon").addClass("active");
-        }
-
-        if($("li.acc-ico, li.acc-ic").hasClass("active")){
-          $("li.acc-ico, li.acc-ic").removeClass("active");
-        }
-      })
-      $("li.acc-ico").click(function(){
-        if((!$("li.acc-ico").hasClass("active"))){
-          $("li.acc-ico").addClass("active");
-        }
-
-        if($("li.acc-icon, li.acc-ic").hasClass("active")){
-          $("li.acc-icon, li.acc-ic").removeClass("active");
-        }
-      })
-      $("li.acc-ic").click(function(){
-        if((!$("li.acc-ic").hasClass("active"))){
-          $("li.acc-ic").addClass("active");
-        }
-
-        if($("li.acc-icon, li.acc-ico").hasClass("active")){
-          $("li.acc-icon, li.acc-ico").removeClass("active");
-        }
-      })
-    })
   }
 }
