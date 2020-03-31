725ab6d8a5076fac2a799c8dced59fdd3aa9358e
cliffordten <tenengclifford5@gmail.com>
2020-03-31 19:59:26 +0100

    added shared dashbord components, header, footer, and sidenave

--- src/app/dashboard/user/user.module.ts
+++ src/app/dashboard/user/user.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { UserComponent } from './user.component';
 import { UserRoutingModule } from './user-routing.module';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from 'src/app/dashboard/shared/header/header.component';
+import { SideNavComponent } from '../shared/side-nav/side-nav.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     UserComponent,
+    HeaderComponent,
+    SideNavComponent,
+    FooterComponent
   ],
   imports: [
-    // BrowserModule,
+    // BrowserModule,NgModule
     UserRoutingModule,
     CommonModule
   ],
