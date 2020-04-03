dca86039b79089a12fb31d1ebaa327793a95d633
cliffordten <tenengclifford5@gmail.com>
2020-04-03 20:07:44 +0100

    modified sidebar, header and routing for the user dashboard

--- src/app/dashboard/user/user-routing.module.ts
+++ src/app/dashboard/user/user-routing.module.ts
@@ -1,9 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user.component';
+import { HomeComponent } from 'src/app/dashboard/user/home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DiabetesTestComponent } from './diabetes-test/diabetes-test.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { AskQuestionComponent } from './ask-question/ask-question.component';
 
 const routes: Routes = [
-  {path: '', component: UserComponent},
+  {path: '', component: UserComponent,
+    children: [
+      {
+        path: 'dashboard', component: HomeComponent
+      },
+      {
+        path: '', redirectTo: 'dashboard', pathMatch: "full"
+      },
+      {
+        path: 'profile', component: ProfileComponent
+      },
+      {
+        path: 'test', component: DiabetesTestComponent
+      },
+      {
+        path: 'statistics', component: StatisticsComponent
+      },
+      {
+        path: 'ask-question', component: AskQuestionComponent
+      }
+    ]},
+
+
 ];
 
 @NgModule({
--- src/app/dashboard/user/user.module.ts
+++ src/app/dashboard/user/user.module.ts
@@ -7,13 +7,23 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from 'src/app/dashboard/shared/header/header.component';
 import { SideNavComponent } from '../shared/side-nav/side-nav.component';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DiabetesTestComponent } from './diabetes-test/diabetes-test.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { AskQuestionComponent } from './ask-question/ask-question.component';
 
 @NgModule({
   declarations: [
     UserComponent,
     HeaderComponent,
     SideNavComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    ProfileComponent,
+    DiabetesTestComponent,
+    StatisticsComponent,
+    AskQuestionComponent
   ],
   imports: [
     // BrowserModule,NgModule
