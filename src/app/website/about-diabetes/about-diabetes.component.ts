2479ec556491fc0f231a29dc6c6b3ebec7d9bb7b
cliffordten <tenengclifford5@gmail.com>
2020-04-30 18:54:34 +0100

    done with about diabetes

---
+++ src/app/website/about-diabetes/about-diabetes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from "jquery";
+
+@Component({
+  selector: 'app-about-diabetes',
+  templateUrl: './about-diabetes.component.html',
+  styleUrls: ['./about-diabetes.component.css']
+})
+export class AboutDiabetesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+
+  }
+
+}
